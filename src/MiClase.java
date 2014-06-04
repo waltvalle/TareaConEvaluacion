23d2a67d85eb4ec40490fc1bb9f908c11ca64ddf
Alumno <alumno@localhost.localdomain>
2014-06-04 08:23:43 -0600

    Tarea con Evaluacion

--- src/MiClase.java
+++ src/MiClase.java
@@ -4,37 +4,56 @@ public class MiClase
 	//devuelve la suma de a y b
 	static int sumarNumeros(int a, int b)
 	{
-		return -1;
+		return (a+b);
 	}
 	
 	//devuelve la resta de a y b
 	static int restarNumeros(int a, int b)
 	{
-		return -1;
+		return (a-b);
 	}
 	
 	//devuelve la multiplicación de a y b
 	static int multiplicarNumeros(int a, int b)
 	{
-		return -1;
+		return (a*b);
 	}
 	
 	//devuelve true si num es par, de lo contrario, devuelve false
 	static boolean esPar(int num)
 	{
+	if ((num%2)==0)
+		return true;
+	else
+	{
 		return false;
 	}
-	
+	}
 	//devuelve el numero mayor entre a, b o c
 	static int getMayor(int a, int b, int c)
 	{
-		return -1;
+		if (a>b && a>c)
+		{
+			return a;
+		}
+		if (b>a && b>c)
+		{
+			return b;
+		}
+		if (c>a && c>b)
+		{
+			return c;
+		}
+		return (a);
 	}
 	
-	//devuelve num elevado a la potencia
+	//devuelve num elevado a la potencia. http://foro.elhacker.net/java/calcular_potencia_en_java-t366409.0.html
 	static int potencia(int num, int potencia)
+	int i=num;
 	{
-		return -1;
+		for (int i=0, i<potencia, i++)
+	{
+		return (potencia=i*num)
 	}
 	
 	//devuelve el numero mayor del arreglo
